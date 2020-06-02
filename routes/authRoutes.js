ea07c3907ea5018f607e35125cefe305c433c3c9
Dipz <dipz@Dipzs-MacBook-Air.local>
2020-06-02 14:36:19 +0100

    logout done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,14 @@ module.exports = (app) => {
   );
 
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  // request is incoming result it outgoing
+  app.get("/api/current_user", (request, result) => {
+    result.send(request.user);
+  });
 };
