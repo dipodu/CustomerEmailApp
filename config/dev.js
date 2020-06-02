6f16de3e3c1fc3c39a84e128f98978d1a7682b13
Dipz <dipz@Dipzs-MacBook-Air.local>
2020-06-02 16:18:49 +0100

    finshed Final oauth flow

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "179817334574-e0557g7ksggo674s1q4g4gmd96ub3e21.apps.googleusercontent.com",
+  googleClientSecret: "KGsXA4SWM7cNoKGStc9hoGcv",
+  mongoURI:
+    "mongodb+srv://dipz999:JBrwaMaasT0OU9KC@customerapp-639d5.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "s8ads98dijn",
+};
