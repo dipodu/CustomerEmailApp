b7ef694fae836f9225a9d0d78040b2d543f79934
Dipz <dipz@dipzs-air.home>
2020-06-05 15:06:06 +0100

    Completed billing and client side app

--- config/dev.js
+++ config/dev.js
@@ -5,4 +5,8 @@ module.exports = {
   mongoURI:
     "mongodb+srv://dipz999:JBrwaMaasT0OU9KC@customerapp-639d5.mongodb.net/test?retryWrites=true&w=majority",
   cookieKey: "s8ads98dijn",
+  stripePublishableKey:
+    "pk_test_51GqItVD3Ox1LV6sjHHTDDUf0Ep5VamC2ukYMlQV7cv8ciJ6svsnXPzSyuyBfAuGKidRUe4fh8vLgh37rRLspe6g000z3bRwAOS",
+  stripeSecretKey:
+    "sk_test_51GqItVD3Ox1LV6sjKjaKLtNQzrBA2P2UHwdvt5QCzRGNxaEDsLrmU6PhrE2F8iKgzaOG9Twy3kRwroXTfmnMkxmM00bQSdLWHi",
 };
